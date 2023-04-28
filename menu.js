73185580a07fcb858285c18bcbda67799cea1eb1
MaykoSlayer <traikmatt1402@gmail.com>
2023-04-27 19:53:39 -0600

    Adding shopping cart, hiding menu when is open and close shopping cart when menu mobile is open

--- menu.js
+++ menu.js
@@ -2,16 +2,33 @@ const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
 const menuMobile = document.querySelector('.menu-mobile');
 const mobileMenu = document.querySelector('.mobile-menu');
+const shoppingCart = document.querySelector('.navbar-shopping-cart');
+const cartMenu = document.querySelector('.product-detail');
+
 
 menuEmail.addEventListener('click', toggleDesktopMenu );
+menuMobile.addEventListener('click', toggleMobileMenu);
+shoppingCart.addEventListener('click', toggleMenuCart);
+
 
 function  toggleDesktopMenu () {
   desktopMenu.classList.toggle('inactive')
 }
 
 
-menuMobile.addEventListener('click', toggleMobileMenu);
-
 function toggleMobileMenu () {
+  const isShoppingCartOpen = !cartMenu.classList.contains('inactive');
+  if(isShoppingCartOpen){
+    cartMenu.classList.add('inactive');
+  }
   mobileMenu.classList.toggle('inactive');
+}
+
+function toggleMenuCart () {
+  const isMenuMobileOpen = !mobileMenu.classList.contains('inactive');
+
+  if(isMenuMobileOpen){
+    mobileMenu.classList.add('inactive');
+  }
+  cartMenu.classList.toggle('inactive');
 }
